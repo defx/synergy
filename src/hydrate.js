d4df5999dde1ed53cf5040e0825973f7d3aba029
defx <mdonkin83@gmail.com>
2020-12-08 13:07:54 +0000

    remove form control fix

--- src/hydrate.js
+++ src/hydrate.js
@@ -23,14 +23,6 @@ const transferBindings = (BINDING_ID, sourceNode, targetNode) => {
         node.__bindings__ = newNode.__bindings__;
         node.bindingId = BINDING_ID;
       }
-
-      if (node.value !== undefined) {
-        if (node.nodeName === 'TEXTAREA') {
-          node.value = node.textContent;
-        } else {
-          node.value = node.getAttribute('value');
-        }
-      }
     }
   });
 
