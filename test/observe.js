2a1c4d5a93f35338f3ede1c4f56620b7a4b5613e
defx <mdonkin83@gmail.com>
2021-01-04 16:59:35 +0000

    fix test

--- test/observe.js
+++ test/observe.js
@@ -1,8 +1,8 @@
-import observe from '../src/observe.js';
+import proxy from '../src/proxy.js';
 
 /*
 
-observe(
+proxy(
   root = {},
   callbackAny,
   callbackObserved = NOOP,
@@ -11,41 +11,32 @@ observe(
 
 */
 
-describe('observe', () => {
+describe('proxy', () => {
   describe('callbackAny', () => {
     it('should callback when new value is added', async () => {
       let count = 0;
-      let value = observe({}, () => count++);
+      let value = proxy({}, () => count++, []);
       value.bar = 'foo';
       await nextFrame();
       assert.equal(count, 1);
     });
     it('should callback when existing value is changed', async () => {
       let count = 0;
-      let value = observe(
-        { foo: 'bar' },
-        () => count++
-      );
+      let value = proxy({ foo: 'bar' }, () => count++, []);
       value.foo = 123;
       await nextFrame();
       assert.equal(count, 1);
     });
     it('should not callback when existing value is set to the same value', async () => {
       let count = 0;
-      let value = observe(
-        { foo: 'bar' },
-        () => count++
-      );
+      let value = proxy({ foo: 'bar' }, () => count++);
       value.foo = 'bar';
       await nextFrame();
       assert.equal(count, 0);
     });
     it('should callback when property is deleted', async () => {
       let count = 0;
-      let value = observe(
-        { foo: 'bar' },
-        () => count++
-      );
+      let value = proxy({ foo: 'bar' }, () => count++);
       delete value.foo;
       await nextFrame();
       assert.equal(count, 1);
@@ -54,7 +45,7 @@ describe('observe', () => {
   describe('callbackObserved', () => {
     it('should callback with name and value', async () => {
       let stack = [];
-      let model = observe(
+      let model = proxy(
         { foo: 'bar' },
         () => {},
         ['foo'],
