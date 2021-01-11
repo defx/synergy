277f94f8e61d752a62e947187a18453d3c4eb629
defx <mdonkin83@gmail.com>
2021-01-11 22:09:04 +0000

    relax hydration comparison further - as long as approximate nodes are found in the same order

--- src/hydrate.js
+++ src/hydrate.js
@@ -1,35 +1,32 @@
 import { walk } from './helpers.js';
 
-const transferBindings = (BINDING_ID, sourceNode, targetNode) => {
-  let nodesByPath = {};
+const transferBindings = (
+  BINDING_ID,
+  sourceNode,
+  targetNode
+) => {
+  let nodes = [];
 
   walk(sourceNode, (node, path) => {
-    if (node.nodeName === 'SLOT') return false; //dont compare this node or its children
-    if (node !== sourceNode) nodesByPath[path] = node;
+    if (node.nodeName === 'SLOT') return false;
+    if (node !== sourceNode) nodes.push(node);
   });
 
-  let shouldHydrate;
-  let count = 0;
+  if (nodes.length === 0) return false;
+
+  let nextNode = nodes.shift();
 
   walk(targetNode, (node, path) => {
-    if (shouldHydrate === false) return false;
-    if (path in nodesByPath) {
-      let newNode = nodesByPath[path];
-
-      shouldHydrate = newNode.nodeName === node.nodeName;
-
-      if (!shouldHydrate) return;
-
-      count++;
-
-      if (newNode.__bindings__) {
-        node.__bindings__ = newNode.__bindings__;
-        node.bindingId = BINDING_ID;
-      }
+    if (!nextNode) return false;
+    if (node.nodeName === nextNode.nodeName) {
+      node.__bindings__ = nextNode.__bindings__;
+      node.__index__ = nextNode.__index__;
+      node.bindingId = BINDING_ID;
+      nextNode = nodes.shift();
     }
   });
 
-  return shouldHydrate && count === Object.keys(nodesByPath).length;
+  return !nextNode;
 };
 
 export default transferBindings;
