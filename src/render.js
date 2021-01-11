34469df2663165866896391a2755f8d07f57aa24
defx <mdonkin83@gmail.com>
2021-01-11 16:39:53 +0000

    remove comments

--- src/render.js
+++ src/render.js
@@ -34,7 +34,6 @@ function render(
   update(templateFragment, viewmodel);
 
   if (hydrate(BINDING_ID, templateFragment, mountNode)) {
-    /* it doesn't have to be a perfect match to hydrate, but we do want to patch the differences. This is an intentional strategy aimed at allowing you to design for users that might have JS turned off by stripping stateful attributes (e.g., [hidden],[disabled],[aria-expanded],etc) from your pre-rendered HTML to avoid dead-end situations where (for example) something is serialised with [hidden] but then there's no JS to unhide it. If your HTML isn't mismatched then this invocation of update won't touch the DOM.  */
     update(mountNode, viewmodel);
   } else {
     if (viewmodel.beforeMountCallback)
