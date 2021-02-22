910ee87a66a405b2a8ff9c6045ddf0c6820a6870
defx <mdonkin83@gmail.com>
2021-02-22 14:57:42 +0000

    there can be only one

--- src/subscribe.js
+++ src/subscribe.js
@@ -1,8 +1,16 @@
-import { getValueAtPath, setValueAtPath } from './helpers.js';
+import {
+  getValueAtPath,
+  setValueAtPath,
+} from './helpers.js';
 
 const inputValue = (node) => {
-  if (node.hasAttribute('multiple') && node.nodeName === 'SELECT') {
-    return Array.from(node.selectedOptions).map((node) => node.value);
+  if (
+    node.hasAttribute('multiple') &&
+    node.nodeName === 'SELECT'
+  ) {
+    return Array.from(node.selectedOptions).map(
+      (node) => node.value
+    );
   }
   let type = node.getAttribute('type') || 'text';
 
@@ -16,42 +24,57 @@ const inputValue = (node) => {
   }
 };
 
-const getEventBindings = (BINDING_ID, type, node) => {
+const getEventBinding = (BINDING_ID, type, node) => {
   if (!node) return;
 
-  let bindings = (
-    (node.bindingId === BINDING_ID && node.__bindings__) ||
-    []
-  ).filter(({ eventName }) => eventName === type);
+  let binding =
+    node.bindingId === BINDING_ID &&
+    node.__bindings__.find(
+      ({ eventName }) => eventName === type
+    );
 
-  return bindings.length
-    ? bindings
-    : getEventBindings(BINDING_ID, type, node.parentNode);
+  return (
+    binding ||
+    getEventBinding(BINDING_ID, type, node.parentNode)
+  );
 };
 
-const subscribe = (rootNode, subscribers, proxy, BINDING_ID) => {
+const subscribe = (
+  rootNode,
+  subscribers,
+  proxy,
+  BINDING_ID
+) => {
   subscribers.forEach((type) => {
     rootNode.addEventListener(
       type,
       (e) => {
-        let bindings = getEventBindings(BINDING_ID, type, e.target);
+        let binding = getEventBinding(
+          BINDING_ID,
+          type,
+          e.target
+        );
 
-        if (!bindings) return;
+        if (!binding) return;
 
-        bindings.forEach((binding) => {
-          if (binding.type === 'call') {
-            let v = binding.realPath && getValueAtPath(binding.realPath, proxy);
+        if (binding.type === 'call') {
+          let v =
+            binding.realPath &&
+            getValueAtPath(binding.realPath, proxy);
 
-            let fn = proxy[binding.method](e, v);
-            if (fn) {
-              requestAnimationFrame(fn);
-            }
+          let fn = proxy[binding.method](e, v);
+          if (fn) {
+            requestAnimationFrame(fn);
           }
+        }
 
-          if (binding.type === 'set') {
-            setValueAtPath(binding.realPath, inputValue(e.target), proxy);
-          }
-        });
+        if (binding.type === 'set') {
+          setValueAtPath(
+            binding.realPath,
+            inputValue(e.target),
+            proxy
+          );
+        }
       },
       type === 'blur' || type === 'focus'
     );
