83f6c49836f68f8846ecb9bfefa1a7566fa0e6e6
defx <mdonkin83@gmail.com>
2021-02-25 09:50:11 +0000

    feat: add declaration file

---
+++ types/index.d.ts
@@ -0,0 +1,41 @@
+export = synergy;
+export as namespace synergy;
+
+declare namespace synergy {
+  interface Hooks {
+    connectedCallback?: (f: (currentState: Model) => void) => void;
+    updatedCallback?: (
+      f: (currentState: Model, previousState: Model) => void
+    ) => void;
+    disconnectedCallback?: (f: (currentState: Model) => void) => void;
+    adoptedCallback?: (f: (currentState: Model) => void) => void;
+  }
+
+  type Model = {
+    [x: string]: any;
+  };
+
+  type ModelFactory = {
+    (initialAttributes: { [x: string]: string | boolean }): Model;
+  };
+
+  function define(
+    name: string,
+    factory: ModelFactory,
+    template: HTMLTemplateElement | string,
+    options: {
+      watch?: Array<string>;
+      shadowRoot?: "open" | "closed";
+      hooks?: Hooks;
+    }
+  ): void;
+
+  function render(
+    mountNode: Node,
+    model: Model,
+    template: HTMLTemplateElement | string,
+    options: {
+      hooks?: Hooks;
+    }
+  ): Model;
+}
