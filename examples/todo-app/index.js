9ea0b8a950eaaa9a48211177f61e64e16af4682c
defx <mdonkin83@gmail.com>
2022-03-25 18:40:01 +0000

    replace reference implementation

--- examples/todo-app/index.js
+++ examples/todo-app/index.js
@@ -1,10 +1,12 @@
 export const storage = {
-  get: (k) => JSON.parse(localStorage.getItem(k)),
-  set: (k, v) => localStorage.setItem(k, JSON.stringify(v)),
+  get: (k) => {
+    let v = JSON.parse(localStorage.getItem(k))
+    return v
+  },
+  set: (k, v) => {
+    localStorage.setItem(k, JSON.stringify(v))
+  },
 }
-
-const html = (strings, ...values) =>
-  strings.reduce((a, v, i) => a + v + (values[i] || ""), "")
 
 const filters = {
   all: (todos) => todos,
@@ -17,84 +19,153 @@ const KEYS = {
   ESCAPE: 27,
 }
 
-export const TodoApp = () => {
-  return {
-    filters: Object.keys(filters),
-    todos: [],
-    activeFilter: "all",
-    addTodo() {
-      let title = this.newTodo && this.newTodo.trim()
-      this.todos.push({ title, id: Date.now() })
-      this.newTodo = null
-    },
-    startEdit(e, item) {
-      this.todos = this.todos.map((todo) => {
-        if (todo === item) {
-          this.titleEdit = item.title
-        }
+const initialState = {
+  filters: Object.keys(filters),
+  todos: [],
+  activeFilter: "all",
+  // filteredTodos: [],
+}
 
-        return {
-          ...todo,
-          editing: todo === item,
+export const middleware = [
+  (action, next) => {
+    switch (action.type) {
+      case "keydown": {
+        const { keyCode } = action.event
+        switch (keyCode) {
+          case KEYS.ESCAPE: {
+            next({ ...action, type: "cancelEdit" })
+            break
+          }
+          case KEYS.RETURN: {
+            next({ ...action, type: "saveEdit" })
+            break
+          }
         }
-      })
-      return () => {
-        e.target.parentNode.querySelector(".edit").focus()
       }
-    },
-    saveEdit(item) {
-      if (!item.editing) return
+      default: {
+        next(action)
+        break
+      }
+    }
+  },
+  (action, next, { afterNextRender }) => {
+    switch (action.type) {
+      case "startEdit": {
+        afterNextRender(() =>
+          action.event.target.parentNode.querySelector(".edit").focus()
+        )
+        next(action)
+        break
+      }
+      default: {
+        next(action)
+      }
+    }
+  },
+]
 
-      item.editing = false
+export const subscribe = (state) => {
+  storage.set("todos", state.todos)
+}
 
-      let title = String(this.titleEdit)
+export const derivations = {
+  allDone: ({ todos }) => todos.every((todo) => todo.completed),
+  filteredTodos: ({ todos, activeFilter }) => filters[activeFilter](todos),
+  numCompleted: ({ todos }) =>
+    todos.filter(({ completed }) => completed).length,
+  itemsLeft: ({ todos }) => {
+    const n = todos.filter(({ completed }) => !completed).length
+    return `${n} item${n === 1 ? "" : "s"} left`
+  },
+}
 
-      if (!title.trim()) {
-        this.todos = this.todos.filter((todo) => {
-          return todo !== item
+export const update = (state = initialState, action) => {
+  const { context, event } = action
+
+  switch (action.type) {
+    case "toggleAll": {
+      let allDone = event.target.checked
+      return {
+        ...state,
+        allDone,
+        todos: state.todos.map((todo) => ({ ...todo, completed: allDone })),
+      }
+    }
+    case "todoInput": {
+      if (event.keyCode === KEYS.RETURN) {
+        return {
+          ...state,
+          todos: state.todos.concat({
+            title: state.newTodo,
+            id: Date.now(),
+            completed: false,
+          }),
+          newTodo: null,
+        }
+      } else {
+        return {
+          ...state,
+          newTodo: title,
+        }
+      }
+    }
+    case "startEdit": {
+      const todos = state.todos.map((todo) => ({
+        ...todo,
+        editing: todo.id === context.todo.id,
+      }))
+      const titleEdit = todos.find((todo) => todo.editing)?.title
+      return {
+        ...state,
+        titleEdit,
+        todos,
+      }
+    }
+    case "deleteTodo": {
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== context.todo.id),
+      }
+    }
+    case "removeCompleted": {
+      return {
+        ...state,
+        todos: state.todos.filter(({ completed }) => !completed),
+      }
+    }
+    case "cancelEdit": {
+      return {
+        ...state,
+        titleEdit: "",
+        todos: state.todos.map((todo) => ({
+          ...todo,
+          editing: false,
+        })),
+      }
+    }
+    case "saveEdit": {
+      let titleEdit = state.titleEdit.trim()
+      let todos
+
+      if (titleEdit) {
+        todos = state.todos.map((todo) => {
+          return {
+            ...todo,
+            title: todo.editing ? state.titleEdit : todo.title,
+            editing: false,
+          }
         })
       } else {
-        item.title = title
+        todos = state.todos.filter(({ editing }) => !editing)
       }
-    },
-    deleteTodo(e, item) {
-      this.todos.splice(this.todos.indexOf(item), 1)
-    },
-    get allDone() {
-      return this.todos.every((todo) => todo.completed)
-    },
-    set allDone(completed) {
-      this.todos = this.todos.map((todo) => ({
-        ...todo,
-        completed,
-      }))
-    },
-    get filteredTodos() {
-      return filters[this.activeFilter](this.todos)
-    },
-    get numCompleted() {
-      return this.todos.filter(({ completed }) => completed).length
-    },
-    removeCompleted() {
-      this.todos = this.todos.filter(({ completed }) => !completed)
-    },
-    get itemsLeft() {
-      const n = this.todos.filter(({ completed }) => !completed).length
-      return `${n} item${n === 1 ? "" : "s"} left`
-    },
-    dispatchKeyDown(e, item) {
-      switch (e.keyCode) {
-        case KEYS.ESCAPE:
-          item.editing = false
-          this.titleEdit = ""
-          break
-        case KEYS.RETURN:
-          this.saveEdit(item)
+
+      return {
+        ...state,
+        todos,
       }
-    },
-    updatedCallback() {
-      storage.set("todos", this.todos)
-    },
+    }
+    default:
+      return { ...state }
   }
 }
 
@@ -124,29 +195,29 @@ export const markup = html`
       autocomplete="off"
       placeholder="What needs to be done?"
       :name="newTodo"
-      :onkeydown="addTodo()"
+      :onkeydown="todoInput"
     />
   </header>
   <main :hidden="{{ !todos.length }}">
     <label for="allDone">Mark all as complete</label>
-    <input id="allDone" type="checkbox" :name="allDone" />
+    <input id="allDone" type="checkbox" :name="allDone" :onchange="toggleAll" />
     <ul>
       <li
         class="todo"
-        :is-done="{{todo.completed}}"
-        :editing="{{todo.editing}}"
+        :is-done="completed"
+        :editing="editing"
         key="id"
         each="todo in filteredTodos"
       >
-        <input class="toggle" type="checkbox" :name="todo.completed" />
-        <label :ondblclick="e => startEdit(e, todo)">{{todo.title}}</label>
+        <input class="toggle" type="checkbox" :name="completed" />
+        <label :ondblclick="startEdit">{{ title }}</label>
         <input
           class="edit"
           :name="titleEdit"
-          :onblur="saveEdit(todo)"
-          :onkeydown="e => dispatchKeyDown(e, todo)"
+          :onblur="saveEdit"
+          :onkeydown="keydown"
         />
-        <button class="delete" :onclick="deleteTodo()">[delete]</button>
+        <button class="delete" :onclick="deleteTodo">[delete]</button>
       </li>
     </ul>
   </main>
@@ -154,16 +225,23 @@ export const markup = html`
     <p id="count">{{ itemsLeft }}</p>
     <ul id="filterList">
       <li each="filter in filters">
-        <input type="radio" :name="activeFilter" :value="{{filter}}" />
+        <input type="radio" :name="activeFilter" :value="filter" />
         <label>{{ filter }}</label>
       </li>
     </ul>
     <button
       id="clearCompleted"
       :hidden="{{ !numCompleted }}"
-      :onclick="removeCompleted()"
+      :onclick="removeCompleted"
     >
       clear completed ({{ numCompleted }})
     </button>
   </footer>
 `
+
+export const factory = () => ({
+  update,
+  middleware,
+  derivations,
+  subscribe,
+})
